9549e9df080d60c0812be63f7e5d225b31a4e058
LukasD <lukas.dvarionis@gmail.com>
2022-09-06 08:54:11 +0300

    Removing unused props

--- redux-anecdotes/src/components/AnecdotesList.js
+++ redux-anecdotes/src/components/AnecdotesList.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseVote } from '../reducers/anecdoteReducer'
 
-const AnecdotesList = (props) => {
+const AnecdotesList = () => {
 
     const dispatch = useDispatch()
     const anecdotes = useSelector(state => state)
