6124e0164276c168a43ddddd0d26f2a90093d426
LukasD <lukas.dvarionis@gmail.com>
2022-09-07 19:55:16 +0300

    6.21 anecdotes, the grand finale Saving previous Notification id of setTimeout which is needed for clearTimeout func
    Added clearTimeout to remove previous notification expire timer when new notification is dispatched

--- redux-anecdotes/src/reducers/notificationReducer.js
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,18 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = {message:'', visible: false}
+const initialState = {message:'', visible: false, previousNotificationId: 0}
 
 const notificationSlice = createSlice({
     name:'notification',
     initialState,
     reducers: {
         setNotification(state, action) {
+            clearTimeout(state.previousNotificationId)
             const content = action.payload
             state.message = content
             state.visible = true
         },
         removeNotification() {
             return initialState
+        },
+        setPreviousNotificationId(state, action) {
+            state.previousNotificationId = action.payload
         }
     }
 })
@@ -20,11 +24,12 @@ const notificationSlice = createSlice({
 export const setExpiringNotification = (text, timeInSecs) => {
     return (dispatch) => {
         dispatch(setNotification(text))
-        setTimeout(() => {
+        const id = setTimeout(() => {
             dispatch(removeNotification())
         }, timeInSecs*1000)
+        dispatch(setPreviousNotificationId(id))
     }
 }
 
-export const { setNotification, removeNotification } = notificationSlice.actions
+export const { setNotification, removeNotification, setPreviousNotificationId } = notificationSlice.actions
 export default notificationSlice.reducer
