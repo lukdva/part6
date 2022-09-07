1b95429e1ed7afcd03791a754036fac13b7027b8
LukasD <lukas.dvarionis@gmail.com>
2022-09-07 19:20:58 +0300

    6.19 anecdotes and connect, step1 Using connect instead of useSelector hook for Notification component

--- redux-anecdotes/src/components/Notification.js
+++ redux-anecdotes/src/components/Notification.js
@@ -1,8 +1,9 @@
 import React from "react"
-import { useSelector } from "react-redux"
+import { connect } from "react-redux"
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
+  console.log(props)
+  const {notification} = props
   const style = {
     border: 'solid',
     padding: 10,
@@ -15,5 +16,10 @@ const Notification = () => {
     </div>
   )
 }
-
-export default Notification
+const mapStateToProps = state => {
+  return {
+    notification: state.notification
+  }
+}
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
